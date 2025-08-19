b58a04d95c12c7bc4e3dae4d1e088c50286f398c
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-08-19 23:35:49 +0330

    ex01-expenses-api

--- core/main.py
+++ core/main.py
@@ -1,150 +1,56 @@
 # main.py
 from fastapi import FastAPI
 
+app = FastAPI()
 
-
-"""
-@app.on_event("startup")
-async def startup_event():
-    print("starting on application.")
-
-
-@app.on_event("shutdown")
-async def startup_event():
-    print("shutting down application.")
-""" 
-
-
-from contextlib import asynccontextmanager
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Application startup.")
-    # e.g. connect DB or Cache Initialization
-    yield
-    # e.g. close DB or clear source 
-    print("Application shutdwon.")
-
-
-app = FastAPI(lifespan=lifespan)
-
-names_list = [
-    {"id": 1, "name": "ali"},
-    {"id": 2, "name": "rose"},
-    {"id": 3, "name": "pedram"},
-    {"id": 4, "name": "aziz"},
-    {"id": 5, "name": "zahra"},
-    {"id": 6, "name": "ali"},
-    {"id": 7, "name": "ali"},
-]
-
-
-# /names (GET(RETRIEVE),POST(CREATE))
-"""
-@app.get("/names")
-def retieve_name_list(q: str | None = None):
-    if q:
-        for item in names_list:
-            return [item for item in names_list if item["name"] == q]
-            # [operation, iteration , condition]
-            # [    O    ,     I     ,     T    ] OIT
-    return names_list
-"""
-
-# or use Optional in  query parameter
-"""
-from typing import Optional
-@app.get("/names")
-def retieve_name_list(q: Optional[str] = None):
-    if q:
-        for item in names_list:
-            return [item for item in names_list if item["name"] == q]
-    return names_list
-"""
-
-# or with Annotation
-"""
-from typing import Annotated
-from fastapi import Query
-@app.get("/names")
-def retieve_name_list(q: Annotated[str | None, Query(max_length=30)] = None):
-    if q:        
-        return [item for item in names_list if item["name"] == q]
-    return names_list
-"""
-
-# or with out annotaions
-from fastapi import Query
-@app.get("/names")
-def retieve_name_list(q: str | None = Query(alias="search", description="it will be searched with the title you inputed",example="ali" , default=None, max_length=30)):
-    if q:        
-        return [item for item in names_list if item["name"] == q]
-    return names_list
+expenses_list = []
 
 
 from fastapi import status, HTTPException, Form, Body
-@app.post("/names", status_code=status.HTTP_201_CREATED)
-def create_name(name: str = Body(embed=True)):
-    id_for_new_name = len(names_list) + 1
-    names_list.append({"id": id_for_new_name, "name": name})
-    return {"detail": "new name created", "new name": names_list[-1]}
+@app.post("/expenses", status_code=status.HTTP_201_CREATED)
+def create_expense(description: str = Body(), amount:int = Body()):
+    id_for_new_expense = len(expenses_list) + 1
+    expenses_list.append({"id": id_for_new_expense, "description": description, "amount": amount})
+    return {"detail": "new expense created", "new expense is": expenses_list[-1]}
 
 
-# /names/:id (GET(RETRIEVE),PUT/PATCH(UPDATE),DELETE)
+from fastapi import Query
+
+@app.get("/expenses")
+def retieve_expense_list(q: str | None = Query(alias="search", description="it will be searched with the title you inputed",example="loan" , default=None, max_length=50)):
+    if q:        
+        return [item for item in expenses_list if item["description"] == q]
+    return expenses_list
+
+
 from fastapi import Path
-@app.get("/names/{name_id}")
-def retrieve_name_detail(name_id: int = Path(alias="name id", title="Name ID", description="this is an integer as name id")):
-    for name in names_list:
-        if name["id"] == name_id:
-            return name
+
+@app.get("/expenses/{expense_id}")
+def retrieve_expense_detail(expense_id: int = Path(alias="expense_id", title="Expense ID", description="this is an integer as expense id")):
+    for item in expenses_list:
+        if item["id"] == expense_id:
+            return item
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="object not found")
 
 
-@app.put("/names/{name_id}", status_code=status.HTTP_200_OK)
-def update_name_detail(name_id: int = Path() , name: str = Form()):
-    for item in names_list:
-        if item["id"] == name_id:
-            item["name"] = name
+@app.put("/expenses/{expense_id}", status_code=status.HTTP_200_OK)
+def update_expense_detail(expense_id: int = Path() , description: str = Body(), amount: int = Body()):
+    for item in expenses_list:
+        if item["id"] == expense_id:
+            item["description"] = description
+            item["amount"] = amount
             return item
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="object not found")
 
 
 from fastapi.responses import JSONResponse
 
-@app.delete("/names/{name_id}")
-def delete_name(name_id: int):
-    for item in names_list:
-        if item["id"] == name_id:
-            names_list.remove(item)
-            names_list[:] = [{"id": i+1, "name": n["name"]}
-                             for i, n in enumerate(names_list)]
+@app.delete("/expenses/{expense_id}")
+def delete_expense(expense_id: int):
+    for item in expenses_list:
+        if item["id"] == expense_id:
+            expenses_list.remove(item)
+            expenses_list[:] = [{"id": i+1, "description": n["description"]} for i, n in enumerate(expenses_list)]
             return JSONResponse(content={"detail": f"{item} deleted!"}, status_code=status.HTTP_200_OK)
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="object not found")
-
-
-@app.get("/")
-def root():
-    msg = {"message": "Hello World!"}
-    return JSONResponse(content=msg, status_code=status.HTTP_202_ACCEPTED)
-
-
-from fastapi import File, UploadFile
-@app.post("/file")
-def file(file: bytes = File(...)):
-    print(file)
-    return {"file_size" : len(file)}
-
-
-@app.post("/uploadfile")
-async def upload_file(file: UploadFile = File(...)):
-    msg = await file.read()
-    print(file.__dict__)
-    return {"filename" : file.filename, "content_type": file.content_type, "file_size": len(msg)}
-
-
-from typing import List
-@app.post("/uploadmultifile")
-async def upload_multi_file(files: List[UploadFile]):
-    return [{"filename" : file.filename, "content_type": file.content_type} for file in files]
-
-
