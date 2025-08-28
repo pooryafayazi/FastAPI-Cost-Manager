9e6570919f4ada153b95f935554ca3ad3073999e
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-08-28 21:09:56 +0330

    13-01

--- core/main.py
+++ core/main.py
@@ -1,9 +1,12 @@
 # core/main.py
 from contextlib import asynccontextmanager
 from fastapi.exceptions import RequestValidationError
+import time
+import random
+from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.security import HTTPBearer
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, BackgroundTasks
 from auth.jwt_auth import get_authenticated_user
 from expenses.routs import router as expenses_router
 from users.routs import router as users_router
@@ -79,3 +82,29 @@ def hello(tr: I18n = Depends(get_translator)):
 app.add_exception_handler(AppError, app_error_handler)
 app.add_exception_handler(StarletteHTTPException, http_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    # print("before")
+    response = await call_next(request)
+    # print("after")
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+task_counter = 1
+
+def start_task(task_id: int):
+    print(f"doing the process: {task_id}")
+    time.sleep(random.randint(3, 10))
+    print(f"finished task {task_id}")
+
+@app.get("/initiate-task", status_code=200)
+async def initiate_task(background_tasks: BackgroundTasks):
+    global task_counter
+    background_tasks.add_task(start_task, task_id=task_counter)
+    task_counter += 1
+    return JSONResponse(content={"detail": "task is done"})
