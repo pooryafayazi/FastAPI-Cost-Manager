5fe5431dbff3d52cbeb18a12b77576c32e187423
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-08-30 14:32:24 +0330

    ex09-loadtest-locust

---
+++ docs/git-pull-from-origin.py
@@ -0,0 +1,18 @@
+# git branch
+
+# git checkout main
+
+# git pull origin main
+
+# git add .
+# git commit -m "local changes"
+# یا
+# git stash
+
+
+# بعد دوباره git pull بزن.
+
+
+# اگه فولدر خالیه و اصلاً repo نداری → ساده‌تره:
+
+# git clone <repo-url>
